713d21f8be43021e96addff1238335fc3bd3e633
ori davidi <oridavidi100@gmail.com>
2022-11-02 12:27:08 +0200

    sets a router for react compoents

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 // import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <div className="">
+          <Routes>
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
