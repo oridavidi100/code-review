addda9eea151a56c05971e147fa8fa8c1cff8f3b
ori davidi <oridavidi100@gmail.com>
2022-11-02 12:05:05 +0200

    cleaning the react files
     and start the files for the work

---
+++ frontend/src/reducer/rootReducer.ts
@@ -0,0 +1,10 @@
+// import { Data } from '../@types/types';
+
+const rootreducer = (state: any, action: any) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default rootreducer;
