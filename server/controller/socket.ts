efca863b86e2feaf0c16b222f738d61edaa31b6c
ori davidi <oridavidi100@gmail.com>
2022-11-03 11:21:30 +0200

    update title backend socket

--- server/controller/socket.ts
+++ server/controller/socket.ts
@@ -4,9 +4,14 @@ import { CodeBlock } from '../db/CodeBlock';
 
 export const onConnection = (socket: SocketType) => {
   socket.on('update', async ({ content, id }) => {
-    console.log(content, id);
     await CodeBlock.findOneAndUpdate({ _id: id }, { content: content });
     socket.broadcast.emit('updateBack', { content });
   });
+  socket.on('updateTitle', async ({ title, id }) => {
+    console.log(title, id);
+    await CodeBlock.findOneAndUpdate({ _id: id }, { title: title });
+    socket.broadcast.emit('updateTitleBack', { title });
+  });
+
   socket.on('disconnect', () => {});
 };
