0b4d2cf724598e532071fb40c4d1b411f3d75029
ori davidi <oridavidi100@gmail.com>
2022-11-03 11:21:39 +0200

    types for update title socket

--- server/@types/socket.ts
+++ server/@types/socket.ts
@@ -2,15 +2,27 @@ import { Socket } from 'socket.io';
 
 export interface ServerToClientEvents {
   updateBack: ({ content }: updateBack) => void;
+  updateTitleBack: ({ title }: UpdateTitleBack) => void;
 }
 
 export interface ClientToServerEvents {
   update: ({ content, id }: update) => void;
+  updateTitle: ({ title, id }: UpdateTitle) => void;
+}
+
+export interface UpdateTitleBack {
+  title: string;
 }
 
 export interface updateBack {
   content: string;
 }
+
+export interface UpdateTitle {
+  title: string;
+  id: string;
+}
+
 export interface update {
   content: string;
   id: string;
