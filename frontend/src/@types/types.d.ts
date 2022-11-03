c1d8ee45625334d4d985c2fefb5fbb9394dcbf89
ori davidi <oridavidi100@gmail.com>
2022-11-03 11:22:49 +0200

    types for frontend socket

--- frontend/src/@types/types.d.ts
+++ frontend/src/@types/types.d.ts
@@ -24,10 +24,21 @@ export declare namespace Data {
 
 export interface ServerToClientEvents {
   updateBack: ({ content }: updateBack) => void;
+  updateTitleBack: ({ title }: UpdateTitleBack) => void;
 }
 
 export interface ClientToServerEvents {
   update: ({ content, id }: update) => void;
+  updateTitle: ({ title, id }: UpdateTitle) => void;
+}
+
+export interface UpdateTitleBack {
+  title: string;
+}
+
+export interface UpdateTitle {
+  title: string;
+  id: string;
 }
 
 export interface updateBack {
